62f6cd46f0517f7c957907798da03176aadde37b
belonosov <belonosov95@gmail.com>
2023-09-05 02:52:54 +0700

    Add chat

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-//let age = prompt("Введите Ваш возраст")
-    if (age >= 18) {
-        alert("Добро пожаловать!")
-    } else {
-        alert("Рекомендуем раздел мультфильмы :)")
+function age() {
+    let age = prompt("Введите Ваш возраст?")
+        if (age >= 18) {
+            alert("Добро пожаловать!")
+        } else {
+            alert("Рекомендуем раздел мультфильмы :)")
         
+        }
     }
+
+function error() {
+    answer = "Привет, "
+    let name = document.getElementById('name').value
+    
+    const result = (answer + name)
+
+    document.getElementById('result').innerText = result
+}
