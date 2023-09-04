5aa7f6cabb8f0d110ce6b72483af623ebaae734f
belonosov <belonosov95@gmail.com>
2023-09-05 03:26:21 +0700

    Add functions

--- js/app.js
+++ js/app.js
@@ -8,11 +8,23 @@ function age() {
         }
     }
 
-function error() {
-    answer = "Привет, "
+function username() {
     let name = document.getElementById('name').value
     
-    const result = (answer + name)
+    console.log(name)
+
+    const result = ("Привет, " + name)
 
     document.getElementById('result').innerText = result
+}
+
+function phone_number() {
+    let number = document.getElementById('number').value
+
+    console.log(number)
+
+    const result2 = ("Менеджер свяжется с Вами по номеру - " + number)
+
+    document.getElementById('result2').innerText = result2
+
 }
